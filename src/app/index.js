5ae6bd8483bbc0981a5c15d06c5200220bc0bf9f
Frank Batista <nybatista@aol.com>
2018-03-13 04:57:33 -0400

    polyfills for ie11

--- src/app/index.js
+++ src/app/index.js
@@ -1,6 +1,7 @@
 import {AppView} from "./components/app/app-view";
 import {SpyneApp, ViewStream, ChannelsBaseData} from 'spynejs';
-import {ChannelData500px} from './channels/channel-data-500px';
+//import {ChannelData500px} from './channels/channel-data-500px';
+require('es6-promise').polyfill();
 
 const css = require("./../scss/main.scss");
 
@@ -54,35 +55,49 @@ const spyneConfig = {
 
 //console.log("CHANNEL CONFIG ",spyneConfig);
 
+window.onStartApp = function() {
 
+    console.log('polys are loaded');
 
-const spyneApp = new SpyneApp(spyneConfig);
-const mapFn = (data)=>{
-    const updates  = (img)=> {
-        img.description = img.description === null
-            ? img.name
-            : img.description;
-        img['perpsectiveNum'] = String((img.height / img.width)*100+"%");
-        return img;
+    const spyneApp = new SpyneApp(spyneConfig);
+    const mapFn = (data) => {
+        const updates = (img) => {
+            img.description = img.description === null
+                ? img.name
+                : img.description;
+            img['perpsectiveNum'] = String((img.height / img.width) * 100 +
+                "%");
+            return img;
+        };
+        data.photos = R.map(updates, data.photos);
+        return data.photos;
     };
-    data.photos = R.map(updates, data.photos);
-    return data.photos;
-};
-const pixData = {
-    dataUrl : "https://api.500px.com/v1/photos/search?term=10&exclude=people&consumer_key=XbScUOttPINmCIoKkeXhRmdBWCM5Nqf0LNZ9Siiv&image_size=5",
-    name : 'ChannelData500px',
-    map: mapFn
-};
+    const pixData = {
+        dataUrl: "https://api.500px.com/v1/photos/search?term=10&exclude=people&consumer_key=XbScUOttPINmCIoKkeXhRmdBWCM5Nqf0LNZ9Siiv&image_size=5",
+        name: 'ChannelData500px',
+        map: mapFn
+    };
 
 //spyneApp.registerChannel('ChannelData500px', new ChannelData500px());
-spyneApp.registerDataChannel(new ChannelsBaseData(pixData));
+    spyneApp.registerDataChannel(new ChannelsBaseData(pixData));
 
-const App = new AppView({
-    el: document.getElementById('example-app')
-});
+    const App = new AppView({
+        el: document.getElementById('example-app')
+    });
 
-const Rx = require('rxjs');
-const R = require("ramda");
-window.R = R;
-window.Rx = Rx;
+    const Rx = require('rxjs');
+    const R = require("ramda");
+    window.R = R;
+    window.Rx = Rx;
+
+}
+
+const onPolysLoaded = ()=>{
+    if (window.polysAreLoaded === true){
+        onStartApp();
+    } else {
+        window.setTimeout(onPolysLoaded, 100);
+    }
+};
+onPolysLoaded();
 
